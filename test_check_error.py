ab0238808b647bf74036764ea3c9f6bc43694dea
pavelkalinchuk <715m4s@gmail.com>
2024-11-19 21:53:03 +0300

    Оптимизация кода. Убрана лишняя фикстура.

--- test_check_error.py
+++ test_check_error.py
@@ -2,16 +2,11 @@ import pytest
 from selene import browser, be, have
 
 
-@pytest.fixture
-def start_tests(browser_start):
-    print("Запускаем тесты!")
-
-
-def test_positive_text(start_tests):
+def test_positive_text(browser_start):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text("yashaka/selene: User-oriented Web UI browser tests in Python"))
 
 
-def test_negative_text(start_tests):
+def test_negative_text(browser_start):
     browser.element('[name="q"]').clear().type('()@#').press_enter()
     browser.element('[id="botstuff"]').should(have.text("did not match any documents"))
